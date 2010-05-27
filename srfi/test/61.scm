3c9986cb12173927f66ac6d9e0d892d1c9939232
Álvaro Castro-Castilla <alvaro@castrocastilla.com>
2010-05-27 02:55:50 +0200

    Improved tests for srfi-61

--- srfi/test/61.scm
+++ srfi/test/61.scm
@@ -1,16 +1,34 @@
 (import ../64)
-(import ../61)
 
-(test-begin "srfi-61" 1)
+(test-begin "srfi-61")
+
+(define this-file "61.scm")
+
+(test-assert #t) ; Bug in srfi-64? Test must start with an assert
 
 ;; From SRFI-61 document
-(define (port->char-list port)
+(define (port->char-list-r5rs port)
   (cond
     ((read-char port) char?
-     => (lambda (c) (cons c (port->char-list port))))
+     => (lambda (c) (cons c (port->char-list-r5rs port))))
     (else '())))
 
-(test-assert
-  (port->char-list (open-input-file "61.scm")))
+(test-error "r5rs-cond"
+  (test-read-eval-string
+    "(port->char-list-r5rs (open-input-file this-file))"))
+
+(import ../61)
+
+(define (port->char-list-srfi port)
+  (cond
+    ((read-char port) char?
+     => (lambda (c) (cons c (port->char-list-srfi port))))
+    (else '())))
+
+;; Tests that it actually expands properly when this srfi is loaded
+
+(test-assert "srfi-cond"
+  (test-read-eval-string
+    "(port->char-list-srfi (open-input-file this-file))"))
 
 (test-end "srfi-61")
