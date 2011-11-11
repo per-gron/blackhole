6cd156282b451ed4cff094838c7763d3f8ad094b
Per Eckerdal <per.eckerdal@gmail.com>
2011-11-11 12:50:18 +0100

    Made compile.sh executable

--- compile.sh
+++ compile.sh
@@ -1,6 +1,5 @@
+#!/bin/sh
 GAMBCLIBDIR="`gsc -e \"(display (path-expand \\\"~~lib\\\"))\"`"
-gsc -link -l "$GAMBCLIBDIR/_gambcgsc.c" bh.scm
-gsc -obj bh_.c bh.c
-gsc -exe -ld-options "$GAMBCLIBDIR/libgambcgsc.a" bh_.o bh.o
-rm bh_.o bh_.c bh.c bh.o
+gsc -link -l "$GAMBCLIBDIR/_gambcgsc.c" bh.scm && gsc -obj bh_.c bh.c && gsc -exe -ld-options "$GAMBCLIBDIR/libgambcgsc.a" bh_.o bh.o
+rm -f bh_.o bh_.c bh.c bh.o
 
