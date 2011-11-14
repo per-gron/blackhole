ef2b5318ed3f0b97856c9262c96ac70d4163cabb
wsxiaoys <wsxiaoys.lh@gmail.com>
2011-11-14 16:46:47 +0800

    ./compile.sh failed with following messages. Fixed by removing the extension.
    bh.scm:
    *** ERROR -- can't open file
    /usr/local/Cellar/gambit-scheme/4.6.1/lib//_gambcgsc.c.c

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 GAMBCLIBDIR="`gsc -e \"(display (path-expand \\\"~~lib\\\"))\"`"
-echo "bh.scm:" && gsc -link -l "$GAMBCLIBDIR/_gambcgsc.c" bh.scm && gsc -obj bh_.c bh.c && gsc -exe -ld-options "$GAMBCLIBDIR/libgambcgsc.a" bh_.o bh.o
+echo "bh.scm:" && gsc -link -l "$GAMBCLIBDIR/_gambcgsc" bh.scm && gsc -obj bh_.c bh.c && gsc -exe -ld-options "$GAMBCLIBDIR/libgambcgsc.a" bh_.o bh.o
 rm -f bh_.o bh_.c bh.c bh.o
 
